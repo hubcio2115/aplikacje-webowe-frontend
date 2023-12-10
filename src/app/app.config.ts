3999613a5c0b883e657b975031b86715ed841848
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-10 23:34:51 +0100

    added necessary providers for app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
-import { ApplicationConfig } from "@angular/core";
+import { provideHttpClient, withFetch } from "@angular/common/http";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideClientHydration } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
+import { provideEffects } from "@ngrx/effects";
+import { provideStore } from "@ngrx/store";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
+
+import { loginEffect, registerEffect } from "~/app/auth/store/auth.store";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideClientHydration()],
+	providers: [
+		provideRouter(routes),
+		provideClientHydration(),
+		provideStore(),
+		provideEffects({ registerEffect, loginEffect }),
+		provideHttpClient(withFetch()),
+		provideStoreDevtools({
+			maxAge: 25,
+			logOnly: !isDevMode(),
+			autoPause: true,
+			trace: false,
+			traceLimit: 75,
+		}),
+	],
 };
