5ffa93f6024bfe5df7722b7e65d35c9200924682
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-18 14:17:58 +0100

    formatting of login-form

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { Component } from "@angular/core";
 import {
 	FormControl,
 	FormGroup,
 	ReactiveFormsModule,
 	Validators,
 } from "@angular/forms";
-import { provideIcons } from "@ng-icons/core";
-import { radixSymbol } from "@ng-icons/radix-icons";
-import { Store } from "@ngrx/store";
 import { HlmButtonDirective } from "@spartan-ng/ui-button-helm";
 import {
 	HlmCardContentDirective,
@@ -18,15 +15,8 @@ import {
 	HlmCardHeaderDirective,
 	HlmCardTitleDirective,
 } from "@spartan-ng/ui-card-helm";
-import { HlmIconComponent } from "@spartan-ng/ui-icon-helm";
 import { HlmInputDirective } from "@spartan-ng/ui-input-helm";
 import { HlmLabelDirective } from "@spartan-ng/ui-label-helm";
-
-import {
-	LoginRequest,
-	authAcitons,
-	selectIsSubmitting,
-} from "~/app/auth/store/auth.store";
 
 @Component({
 	selector: "app-login-form",
@@ -38,7 +28,6 @@ import {
 		HlmCardFooterDirective,
 		HlmCardHeaderDirective,
 		HlmCardTitleDirective,
-		HlmIconComponent,
 
 		HlmInputDirective,
 		HlmLabelDirective,
@@ -47,18 +36,14 @@ import {
 		ReactiveFormsModule,
 		CommonModule,
 	],
-	providers: [provideIcons({ radixSymbol })],
 	templateUrl: "./login-form.component.html",
 })
 export class LoginFormComponent {
-	#store = inject(Store);
-	isSubmitting$ = this.#store.select(selectIsSubmitting);
-
 	loginForm = new FormGroup({
 		email: new FormControl("", {
 			nonNullable: true,
 			updateOn: "submit",
-			validators: [Validators.email],
+			validators: [Validators.required, Validators.email],
 		}),
 		password: new FormControl("", {
 			nonNullable: true,
@@ -67,9 +52,5 @@ export class LoginFormComponent {
 		}),
 	});
 
-	onSubmit() {
-		this.#store.dispatch(
-			authAcitons.login({ request: this.loginForm.value as LoginRequest }),
-		);
-	}
+	onSubmit() {}
 }
