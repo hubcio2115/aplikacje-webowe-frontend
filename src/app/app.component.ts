e17ce313360c3d84e9dc2322c1bf86a18235148f
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-18 14:20:57 +0100

    added angular-query providers with devtools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AngularQueryDevtools } from "@tanstack/angular-query-devtools-experimental";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [CommonModule, RouterOutlet],
+	imports: [CommonModule, RouterOutlet, AngularQueryDevtools],
 	templateUrl: "./app.component.html",
 })
-export class AppComponent {
-	title = "frontend";
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,10 @@
-import { provideHttpClient, withFetch } from "@angular/common/http";
-import { ApplicationConfig, isDevMode } from "@angular/core";
+import { ApplicationConfig } from "@angular/core";
 import { provideClientHydration } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
-import { provideEffects } from "@ngrx/effects";
-import { provideStore } from "@ngrx/store";
-import { provideStoreDevtools } from "@ngrx/store-devtools";
-
-import { loginEffect, registerEffect } from "~/app/auth/store/auth.store";
+import {
+	QueryClient,
+	provideAngularQuery,
+} from "@tanstack/angular-query-experimental";
 
 import { routes } from "./app.routes";
 
@@ -14,15 +12,6 @@ export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideClientHydration(),
-		provideStore(),
-		provideEffects({ registerEffect, loginEffect }),
-		provideHttpClient(withFetch()),
-		provideStoreDevtools({
-			maxAge: 25,
-			logOnly: !isDevMode(),
-			autoPause: true,
-			trace: false,
-			traceLimit: 75,
-		}),
+		provideAngularQuery(new QueryClient()),
 	],
 };
