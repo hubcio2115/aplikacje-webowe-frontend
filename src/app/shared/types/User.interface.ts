71bb1ab1df7da480c5d64db5e5a5b99586d34caa
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-20 21:17:05 +0100

    added userSchema from zod

--- src/app/shared/types/User.interface.ts
+++ src/app/shared/types/User.interface.ts
@@ -1,6 +1,10 @@
-export type User = {
-	id: number;
-	firstName: string;
-	lastName: string;
-	email: string;
-};
+import { z } from "zod";
+
+export const userSchema = z.object({
+	id: z.number(),
+	firstName: z.string(),
+	lastName: z.string(),
+	email: z.string().email(),
+});
+
+export type User = z.infer<typeof userSchema>;
