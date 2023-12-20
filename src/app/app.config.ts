9721f9489e3a6569aa8cbffa38777e93f1e02232
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-20 21:16:26 +0100

    deleted angular-query providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,9 @@
+import { provideHttpClient, withFetch } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
-import {
-	QueryClient,
-	provideAngularQuery,
-} from "@tanstack/angular-query-experimental";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideAngularQuery(new QueryClient())],
+	providers: [provideHttpClient(withFetch()), provideRouter(routes)],
 };
