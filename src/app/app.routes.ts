0c9eb796b40a96ca9fc545d0e0408427b47e1ad9
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-20 21:16:38 +0100

    added HomeComponent to the root route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from "@angular/router";
 
 import { AuthComponent } from "./pages/auth/auth.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
 	{
 		path: "",
-		pathMatch: "full",
-		redirectTo: "auth?tab=login",
+		component: HomeComponent,
 	},
 	{
 		path: "auth",
