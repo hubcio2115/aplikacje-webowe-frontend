89e30e7e933133e300c2af393cfa999a197d362d
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-30 16:33:41 +0100

    added Home, CreateCountry, Details, PageNotFound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,41 @@
 import { Routes } from "@angular/router";
 
 import { AuthComponent } from "./pages/auth/auth.component";
+import { CreateCountryComponent } from "./pages/create-country/create-country.component";
+import { DetailsComponent } from "./pages/details/details.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { countryResolver } from "./shared/resolvers/country.resolver";
+import { rulerResolver } from "./shared/resolvers/ruler.resolver";
 
 export const routes: Routes = [
 	{
 		path: "",
-		component: HomeComponent,
+		redirectTo: "countries",
+		pathMatch: "full",
 	},
 	{
 		path: "auth",
 		component: AuthComponent,
+	},
+	{
+		path: "countries",
+		component: HomeComponent,
+	},
+	{
+		path: "countries/create",
+		component: CreateCountryComponent,
+	},
+	{
+		path: "countries/:id",
+		resolve: {
+			country: countryResolver,
+			ruler: rulerResolver,
+		},
+		component: DetailsComponent,
+	},
+	{
+		path: "**",
+		component: PageNotFoundComponent,
 	},
 ];
