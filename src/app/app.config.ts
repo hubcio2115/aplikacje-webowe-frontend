ffc21ca80d2ed502f59fc7e3c0409953e5b0e5b4
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-12-30 16:33:14 +0100

    added query-provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { provideHttpClient, withFetch } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import {
+	QueryClient,
+	provideAngularQuery,
+} from "@tanstack/angular-query-experimental";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideHttpClient(withFetch()), provideRouter(routes)],
+	providers: [
+		provideHttpClient(withFetch()),
+		provideAngularQuery(new QueryClient()),
+		provideRouter(routes),
+	],
 };
