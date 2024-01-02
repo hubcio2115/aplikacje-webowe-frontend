b9dee64a7606972d2378d2a43782d2300f0a4a4f
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-02 14:31:50 +0100

    added guards to the router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from "@angular/router";
 
+import { NavbarComponent } from "./layouts/navbar/navbar.component";
 import { AuthComponent } from "./pages/auth/auth.component";
 import { CreateCountryComponent } from "./pages/create-country/create-country.component";
 import { DetailsComponent } from "./pages/details/details.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { UserDetailsComponent } from "./pages/user-details/user-details.component";
+import { isAuthedGuard } from "./shared/guards/is-authed.guard";
+import { isNotAuthedGuard } from "./shared/guards/is-not-authed.guards";
 import { countryResolver } from "./shared/resolvers/country.resolver";
 import { rulerResolver } from "./shared/resolvers/ruler.resolver";
 
@@ -17,22 +21,34 @@ export const routes: Routes = [
 	{
 		path: "auth",
 		component: AuthComponent,
+		canActivate: [isNotAuthedGuard],
 	},
 	{
 		path: "countries",
-		component: HomeComponent,
+		component: NavbarComponent,
+		children: [
+			{
+				path: "",
+				component: HomeComponent,
+			},
+			{
+				path: "create",
+				component: CreateCountryComponent,
+				canActivate: [isAuthedGuard],
+			},
+			{
+				path: ":id",
+				resolve: {
+					country: countryResolver,
+					ruler: rulerResolver,
+				},
+				component: DetailsComponent,
+			},
+		],
 	},
 	{
-		path: "countries/create",
-		component: CreateCountryComponent,
-	},
-	{
-		path: "countries/:id",
-		resolve: {
-			country: countryResolver,
-			ruler: rulerResolver,
-		},
-		component: DetailsComponent,
+		path: "user-details",
+		component: UserDetailsComponent,
 	},
 	{
 		path: "**",
